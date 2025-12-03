f33de348e76fba4a037a6877c153022083e4f3e1
Tahagkdr <250541092@firat.edu.tr>
2025-12-03 22:55:15 +0300

    Refactor Exercise4_6_Multadd to Multadd class

--- Exercise4_6_Multadd.java
+++ Exercise4_6_Multadd.java
@@ -1,16 +1,41 @@
-public class Exercise4_6_Multadd {
-
-    public static double multadd(double a, double b, double c) {
-        // TODO
-        return 0.0;
-    }
-
-    public static double expSum(double x) {
-        // TODO
-        return 0.0;
-    }
+public class Multadd {
 
     public static void main(String[] args) {
-        // TODO: test methods
+        
+       
+        System.out.println("Multadd Test (1, 2, 3): " + multadd(1.0, 2.0, 3.0));
+
+       
+        
+        final double PI_OVER_4 = Math.PI / 4;
+        final double HALF_FACTOR = 0.5;
+        
+        double sinVal = Math.sin(PI_OVER_4);
+        double cosVal = Math.cos(PI_OVER_4);
+        
+        double resultTrig = multadd(cosVal, HALF_FACTOR, sinVal);
+        System.out.println("Trigonometri Sonucu: " + resultTrig);
+
+       
+        double resultLog = multadd(1.0, Math.log(10), Math.log(20));
+        System.out.println("Logaritma Sonucu: " + resultLog);
+
+      
+        System.out.println("expSum(2.0) Sonucu: " + expSum(2.0));
+    }
+
+   
+    public static double multadd(double a, double b, double c) {
+        return a * b + c;
+    }
+
+   
+    public static double expSum(double x) {
+       
+        
+        final double exponentialPart = Math.exp(-x);
+        final double sqrtPart = Math.sqrt(1.0 - exponentialPart);
+        
+        return multadd(x, exponentialPart, sqrtPart);
     }
 }
